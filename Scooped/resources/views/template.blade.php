d338be7e82ca595ca3241d7aadd1dc648f144f3c
Erik <erik.henry@dcmail.ca>
2019-02-13 12:24:21 -0500

    homepage

--- Scooped/resources/views/template.blade.php
+++ Scooped/resources/views/template.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Product example · Bootstrap</title>
+    <title>Scooped</title>
 
     <!-- Bootstrap core CSS -->
 <link href="https://getbootstrap.com/docs/4.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
@@ -30,20 +30,18 @@
     <a class="py-2" href="#">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto" focusable="false" role="img"><title>Product</title><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
     </a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Product</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Support</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Pricing</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Home</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Drivers</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Ride</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">History</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Login</a>
   </div>
 </nav>
 
 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
   <div class="col-md-5 p-lg-5 mx-auto my-5">
-    <h1 class="display-4 font-weight-normal">Punny headline</h1>
-    <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
+    <h1 class="display-4 font-weight-normal">Scooped</h1>
+    <p class="lead font-weight-normal">A platform for students to connect and rideshare commuting to school</p>
     <a class="btn btn-outline-secondary" href="#">Coming soon</a>
   </div>
   <div class="product-device shadow-sm d-none d-md-block"></div>
