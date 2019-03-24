6ba6d6022fd5240bc316f82bb9b2ffb2cce7b930
Erik <erik.henry@dcmail.ca>
2019-03-24 15:48:48 -0400

    ride pages

---
+++ Scooped/resources/views/requirementsride.blade.php
@@ -0,0 +1,79 @@
+@include ('partials.navbar')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Riders</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+		<link href="https://getbootstrap.com/docs/4.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+
+        <!-- Styles -->
+        <style>
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            li {
+                  text-align: left;
+                }
+
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+					<div class="title m-b-md">
+                        Requirements
+                    </div>
+                    <br/>
+                    <br/>
+					<p><b>In order to become a rider for Scooped, you must meet the following requirements</b>
+                    <br/><br/>
+                    <ul>
+					   <li>Be a Durham College student</li>
+					   <li>Have a valid G level provincial driver's license</li>
+                       <li>Be using an eligible vehicle</li>
+                    </ul>
+                    </p>
+                    <br/><br/><br/>
+					<a class="btn btn-outline-secondary text-black " href="login">Get Started</a>
+            </div>
+        </div>
+    </body>
+</html>
